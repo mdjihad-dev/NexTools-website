a65a17e1cfd929a3eba106711300cce895e28411
MD JIHAD <admin@gmail.com>
2026-04-27 09:50:27 +0600

    added responsive navbar section

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
+
+import Digitools from '../assets/Image/design-tools.png'
+import { IoCartOutline } from 'react-icons/io5';
 
 const Navbar = () => {
-    return (
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Link</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
+  return (
+    <div className="my-5 flex justify-between items-center px-6 bg-base-100 shadow-sm max-w-7xl mx-auto">
+      <div className="">
+        <img
+          className="w-36 h-auto object-cover"
+          src={Digitools}
+          alt="digitools image"
+        />
       </div>
-    );
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 flex items-center gap-5">
+          <li>
+            <a className="text-base text-[#101727] font-semibold">Products</a>
+          </li>
+          <li>
+            <a className="text-base text-[#101727] font-semibold">Features</a>
+          </li>
+          <li>
+            <a className="text-base text-[#101727] font-semibold">Pricing</a>
+          </li>
+          <li>
+            <a className="text-base text-[#101727] font-semibold">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="text-base text-[#101727] font-semibold">FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center gap-3.5">
+        <IoCartOutline className='w-6 h-auto'/>
+        <button className="btn">Login</button>
+        <button className="btn bg-linear-to-r bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-3xl">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
